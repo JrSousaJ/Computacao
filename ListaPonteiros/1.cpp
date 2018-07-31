520fa93254be2586f1ce46c57b41a5592bf8803e
SOUSA,J <35965251+JrSousaJ@users.noreply.github.com>
2018-07-30 21:11:43 -0300

    Update 1.cpp

--- ListaPonteiros/1.cpp
+++ ListaPonteiros/1.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+//corrigir questão do vetor pra armazenar os dados
+using namespace std;
 
+int fatr(int v[10],int n,int *pont)
+{
+    int c=0;
+    int c1=0;
+    while(n!=1)
+    {
+        while(n%v[c]==0)
+        {
+            n=n/v[c];
+            c1++;
+        }
+            c++;
+    }
+    *pont=c1;
+    if(*pont>10)return 1;
+    else return 0;
+}
+
+int main()
+{
+    int x[10]={2,3,5,7,11,13,17,19,23,29};
+    int n;
+    int z;
+    scanf("%d",&n);
+
+    cout << fatr(x,n,&z) << endl;
+
+    return 0;
+}
