8428e970134492403cb7bbc30e0fdcee42f42dc9
SOUSA,J <35965251+JrSousaJ@users.noreply.github.com>
2018-07-31 20:00:28 -0300

    Update 1.cpp

--- ListaPonteiros/1.cpp
+++ ListaPonteiros/1.cpp
@@ -1,17 +1,16 @@
 #include<bits/stdc++.h>
-//corrigir questão do vetor pra armazenar os dados da decomposicao
 using namespace std;
-
-int fatr(int v[10],int n,int *pont)
+int x[10]={2,3,5,7,11,13,17,19,23,29};
+int fatr(int v[101],int n,int *pont)
 {
     int c=0;
     int c1=0;
     while(n!=1)
     {
-        while(n%v[c]==0)
+        while(n%x[c]==0)
         {
-            n=n/v[c];
-            c1++;
+            n/=x[c];
+            v[c1++]=x[c];
         }
             c++;
     }
@@ -22,12 +21,11 @@ int fatr(int v[10],int n,int *pont)
 
 int main()
 {
-    int x[10]={2,3,5,7,11,13,17,19,23,29};
-    int n;
+    int n,b[101];
     int z;
     scanf("%d",&n);
 
-    cout << fatr(x,n,&z) << endl;
+    cout << fatr(b,n,&z) << endl;
 
     return 0;
 }
