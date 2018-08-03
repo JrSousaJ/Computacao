25520d8d66402a51379a146d207189815d3391ec
SOUSA,J <35965251+JrSousaJ@users.noreply.github.com>
2018-08-03 16:25:59 -0300

    Update 7.cpp

--- ListaPonteiros/7.cpp
+++ ListaPonteiros/7.cpp
@@ -11,6 +11,8 @@ int main()
 {
     
     scanf("%d%d",&n,&n2);
+    /*deve ser passado por referencia pois a função deveria ter 2 retornos, o X e Y, usando ponteiros eu posso retornar os 2
+    valores nas variaveis passadas por referencia ao ponteiro*/
     calcula(&n,&n2);
     printf("%d %d\n",n,n2);
     return 0;
