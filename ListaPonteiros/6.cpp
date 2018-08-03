53c47c45fd0fcdfab15dfc64ff82321d02a8ca1c
SOUSA,J <35965251+JrSousaJ@users.noreply.github.com>
2018-08-03 15:55:50 -0300

    Create 6.cpp

---
+++ ListaPonteiros/6.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int v[3];
+int prim,seg,terc;
+void ord(int *a,int *b,int *c)
+{
+    prim=min((int)*a,min((int)*b,(int)*c));
+    terc=max((int)*a,max((int)*b,(int)*c));
+    if(*a!=prim && *a!=terc)seg=*a;
+    else if(*b!=prim && *b!=terc)seg=*b;
+    else if(*c!=prim && *c!=terc)seg=*c;
+    *a=prim;
+    *b=seg;
+    *c=terc;
+}
+int main()
+{
+    for(int i=0;i<3;i++)scanf("%d",&v[i]);
+    ord(&v[0],&v[1],&v[2]);
+    printf("A:%d, B:%d, C:%d\n",v[0],v[1],v[2]);
+    return 0;
+}
