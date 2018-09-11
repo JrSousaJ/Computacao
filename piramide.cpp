0e9e8cae9b7ba0f6f49a6a5bb8960c9c3ebd2487
SOUSA,J <35965251+JrSousaJ@users.noreply.github.com>
2018-09-11 19:00:40 -0300

    Create piramide.cpp

---
+++ piramide.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  while(scanf("%lf%lf%lf%lf",&a,&b,&c,&d))
+  {
+  if(!a && !b && !c && !d)break;
+  double k=((a/2)+b)/d;
+  double ans=k*c;
+   printf("%.5lf\n",ans);s
+  }
+}
