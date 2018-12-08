b297ba2efc5871272d5fab4f9dc338b32292e14f
SOUSA,J <35965251+JrSousaJ@users.noreply.github.com>
2018-12-08 19:13:29 -0200

    Create 4.cpp

---
+++ binary_tree/4.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+struct no
+{
+    struct no *esq;
+    struct no *dir;
+    int n;
+};
+typedef struct no *Arvore;
+void insertNode(Arvore *t, int d)
+{
+    if (*t == NULL)
+    {
+        *t = (struct no*) malloc(sizeof(struct no));
+    if ( *t != NULL )
+    {
+        (*t)->esq = NULL;
+        (*t)->dir = NULL;
+        (*t)->n = d;
+    }
+    else
+        printf("Memoria insuficiente");
+    }
+    else
+        if (d < (*t)->n)
+            insertNode(&(*t)->esq, d);
+    else
+        if (d > (*t)->n)
+            insertNode(&(*t)->dir, d);
+        else
+            printf("Duplicacao de no");
+}
+void preorder(Arvore t)
+{
+    if(t!=NULL)
+    {
+        printf("%c\n",t->n);
+        preorder(t->esq);
+        preorder(t->dir);
+    }
+}
+void inOrder(Arvore t)
+{
+    if(t!=NULL)
+    {
+        inOrder(t->esq);
+        printf("%c", t->n);
+        inOrder(t->dir);
+    }
+}
+void posOrder(Arvore t)
+{
+    if (t != NULL)
+    {
+        posOrder(t->esq);
+        posOrder(t->dir);
+        printf("%c", t->n);
+    }
+}
+void ContaFolhas(Arvore t) 
+{
+    if(t==NULL)
+       return ;
+    if (t->esq == NULL && t->dir == NULL)printf("Folha: %d\n",t->n);
+    ContaFolhas(t->esq);
+    ContaFolhas(t->dir);
+}
+int main()
+{
+    Arvore a = NULL;
+    printf("Quer inserir quantos valores? \n");
+    int x;scanf("%d",&x);
+    while(x--)
+    {
+        int aa;scanf("%d",&aa);
+        insertNode(&a,aa);
+    }
+    ContaFolhas(a);
+}
