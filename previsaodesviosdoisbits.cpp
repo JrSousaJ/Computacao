10ddbe73805225ad95bb4a27d7374051f9f836b4
SOUSA,J <35965251+JrSousaJ@users.noreply.github.com>
2019-11-19 20:36:56 -0300

    Create previsaodesviosdoisbits.cpp

---
+++ previsaodesviosdoisbits.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+/*
+    Algoritmo de previsão dinâmica de desvios considerando 2 bits.
+    Aluno: Jairo de Sousa Júnior.
+    4º Período - IFSULDEMINAS.
+    Disciplina: Arquitetura e Organização de Computadores.
+*/
+int main()
+{
+    FILE *entrada;
+    //Entrada de dados pelo arquivo dados.txt
+    entrada = fopen("dados.txt", "r");
+    char s[100000];
+    //Leitura do arquivo
+    fscanf(entrada," %s",&s);
+    fclose(entrada);
+    //Encerramento da entrada de dados.
+    //Inicialização das variáveis de acertos e erros.
+    int e=0,a=0;
+    int contador = 0;
+    int est = 0;
+    //Mapa só para controlar os true e false.
+    map<char,int>mapa;
+    mapa['N'] = 0;//0 é falso.
+    mapa['T'] = 1;//1 é true.
+    //Laço para percorrer a string.
+    for(int i=0;i<strlen(s);i++)
+    {
+        //Se o estado atual for igual á letra atual, o número de acertos aumenta e o contador zera.
+        if(mapa[s[i]] == est)
+        {
+            a++;
+            contador=0;
+        }
+        else //Se o estado atual for diferente da letra atual, o número de erros aumenta e o contador de erros aumenta,
+        {
+            e++;
+            contador++;
+        }
+        if(contador==2)//Se o meu contador chegar a dois, ele muda de estado e zera o contador.
+        {
+            est =(est+1)%2;
+            contador=0;
+        }
+    }
+    printf("Numero de acertos: %d\n",a);
+    printf("Numero de erros: %d\n",e);
+
+    return 0;
+}
