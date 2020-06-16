eb77e86b2502af3831c765d38b55d515435c792e
SOUSA,J <35965251+JrSousaJ@users.noreply.github.com>
2020-06-16 20:10:35 -0300

    Update caixeiro.cpp

--- caixeiro.cpp
+++ caixeiro.cpp
@@ -1,21 +1,34 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+//Definicão de "infinito" para valores inteiros.
 const int oo = 0x3f3f3f3f;
 int g[100][100],n;
-int memo[n][1<<n];
+//Matriz de memorização
+//1LL << n tem como função jogar 1 bit à n posições para esquerda.
+int memo[n][1LL<<n];
 int pd(int v,int mask)
 {
-	if(__builtin_popcount(mask)==n)return g[v][0];
+	//Verificação se já passei por todas as cidades.
+	if(__builtin_popcount(mask)==n)
+		return g[v][0];
+	//Defino minha resposta inicial como "infinito".
 	int ans = oo;
-	if(memo[v][mask]!=-1)return memo[v][mask];
+	//Verifico se já calculei anteriormente o meu estado atual.
+	if(memo[v][mask]!=-1)
+		return memo[v][mask];
+	//Percorro as cidades.
 	for(int i=0;i<n;i++)
 	{
+		//Verifico se a cidade atual já foi visitada.
 		if(!((mask>>i)&1))
 		{
+			//Chamada recursiva buscando o menor valor possível entre a resposta atual
+			//e a da próxima chamada recursiva.
 			ans=min(ans,pd(i,mask|1<<i)+g[v][i])
 		}
 	}
+	//Retorno minha resposta para minha matriz de memorização e para minha pilha recursiva.
 	return memo[v][mask]=ans;
 }
 
